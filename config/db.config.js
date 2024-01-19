7a3067db64df12dba36a566e6d2870c5eafe4bbf
v.kumar.ambeshta <v.kumar.ambeshta@accenture.com>
2024-01-19 14:12:11 +0530

    db

--- config/db.config.js
+++ config/db.config.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 // Allow Promises
 mongoose.Promise = global.Promise;
 // Connection
-// mongoose.connect('mongodb://127.0.0.1:27017/employee-db');
-mongoose.connect('mongodb://mongodb-cluster-ip-serv/employee-db');
+mongoose.connect('mongodb://127.0.0.1:27017/employee-db');
+// mongoose.connect('mongodb://mongodb-cluster-ip-serv/employee-db');
 // Validation
 mongoose.connection
   .once('open', () => console.log('Connected to the database!'))
